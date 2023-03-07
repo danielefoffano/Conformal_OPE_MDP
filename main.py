ae93613243dc3eaa7cb2316617177fd5ddb32cf9
Alessio Russo <russo.alessio@outlook.com>
2023-03-07 10:51:10 +0100

    add horizons

--- main.py
+++ main.py
@@ -43,120 +43,121 @@ if __name__ == "__main__":
     ALPHA = 0.6                                                                         # behaviour agent alpha
     NUM_STEPS = 20000                                                                   # behaviour agent learning steps
     N_TRAJECTORIES = 20000                                                              # number of trajectories collected as dataset
-    HORIZON = 10                                                                        # trajectory horizon
+    HORIZONS = np.geomspace(3, 25, num=8, dtype=int)                                    # trajectory horizon
+    for HORIZON in HORIZONS:
+        print(f'Starting with horizon: {HORIZON}')
+        method = "gradient_based" if GRADIENT_BASED else "model_based"
+        columns = ["epsilon", "Coverage", "Avg_length", "Original_interval_bottom", "Original_interval_upper", "horizon", "epsilon_pi_b"]
+        path = f"results/{ENV_NAME}/{method}/horizon_{HORIZON}/"
 
-    method = "gradient_based" if GRADIENT_BASED else "model_based"
-    columns = ["epsilon", "Coverage", "Avg_length", "Original_interval_bottom", "Original_interval_upper", "horizon", "epsilon_pi_b"]
-    path = f"results/{ENV_NAME}/{method}/horizon_{HORIZON}/"
+        for RUN_NUMBER in range(RUNS_NUMBER):
+            file_logger = Logger(path+f"run_{RUN_NUMBER}", columns)
 
-    for RUN_NUMBER in range(RUNS_NUMBER):
-        file_logger = Logger(path+f"run_{RUN_NUMBER}", columns)
+            P = np.random.dirichlet(np.ones(NUM_STATES), size=(NUM_STATES, NUM_ACTIONS))        # MDP transition probability functions
+            if ENV_NAME == "random_mdp":
+                if REWARD_TYPE == "bernoulli":
+                    R = np.random.rand(NUM_STATES, NUM_ACTIONS)                                     # MDP reward function Bernoulli
+                    env = MDPEnvBernoulliRew(NUM_STATES, NUM_ACTIONS, P, R)
+                    model = DiscreteRewardDynamicsModel(NUM_STATES, NUM_ACTIONS, NUM_REWARDS)
+                elif REWARD_TYPE == "discrete_multiple":                                 
+                    R = np.random.dirichlet(np.ones(NUM_REWARDS), size=(NUM_STATES, NUM_ACTIONS))   # MDP reward function multiple discrete r values
+                    env = MDPEnvDiscreteRew(NUM_STATES, NUM_ACTIONS, NUM_REWARDS, P, R)
+                    model = DiscreteRewardDynamicsModel(NUM_STATES, NUM_ACTIONS, NUM_REWARDS)
+                elif REWARD_TYPE == "continuous":
+                    R = np.random.rand(NUM_STATES, NUM_ACTIONS, NUM_STATES)
+                    env = MDPEnv(NUM_STATES, NUM_ACTIONS, P, R)
+                    #model = DiscreteRewardDynamicsModel(NUM_STATES, NUM_ACTIONS, NUM_REWARDS)
+                    model = ContinuousRewardDynamicsModel(NUM_STATES, NUM_ACTIONS)
+                
+                pi_star_probs = np.random.dirichlet(np.ones(NUM_ACTIONS), size=(NUM_STATES))
+                pi_star_pre = TableBasedPolicy(pi_star_probs)
+                
+                #Train behaviour policy using Q-learning
+                agent = QlearningAgent(NUM_STATES, NUM_ACTIONS, DISCOUNT_FACTOR, ALPHA)
 
-        P = np.random.dirichlet(np.ones(NUM_STATES), size=(NUM_STATES, NUM_ACTIONS))        # MDP transition probability functions
-        if ENV_NAME == "random_mdp":
-            if REWARD_TYPE == "bernoulli":
-                R = np.random.rand(NUM_STATES, NUM_ACTIONS)                                     # MDP reward function Bernoulli
-                env = MDPEnvBernoulliRew(NUM_STATES, NUM_ACTIONS, P, R)
-                model = DiscreteRewardDynamicsModel(NUM_STATES, NUM_ACTIONS, NUM_REWARDS)
-            elif REWARD_TYPE == "discrete_multiple":                                 
-                R = np.random.dirichlet(np.ones(NUM_REWARDS), size=(NUM_STATES, NUM_ACTIONS))   # MDP reward function multiple discrete r values
-                env = MDPEnvDiscreteRew(NUM_STATES, NUM_ACTIONS, NUM_REWARDS, P, R)
-                model = DiscreteRewardDynamicsModel(NUM_STATES, NUM_ACTIONS, NUM_REWARDS)
-            elif REWARD_TYPE == "continuous":
-                R = np.random.rand(NUM_STATES, NUM_ACTIONS, NUM_STATES)
-                env = MDPEnv(NUM_STATES, NUM_ACTIONS, P, R)
-                #model = DiscreteRewardDynamicsModel(NUM_STATES, NUM_ACTIONS, NUM_REWARDS)
-                model = ContinuousRewardDynamicsModel(NUM_STATES, NUM_ACTIONS)
-            
-            pi_star_probs = np.random.dirichlet(np.ones(NUM_ACTIONS), size=(NUM_STATES))
-            pi_star_pre = TableBasedPolicy(pi_star_probs)
-            
-            #Train behaviour policy using Q-learning
-            agent = QlearningAgent(NUM_STATES, NUM_ACTIONS, DISCOUNT_FACTOR, ALPHA)
+                q_table = train_behaviour_policy(env, agent, NUM_STEPS)
+                behaviour_policy = EpsilonGreedyPolicy(q_table, EPSILON, NUM_ACTIONS)
 
-            q_table = train_behaviour_policy(env, agent, NUM_STEPS)
-            behaviour_policy = EpsilonGreedyPolicy(q_table, EPSILON, NUM_ACTIONS)
+            elif ENV_NAME == "inventory":
+                env = Inventory(inventory_size = NUM_STATES, fixed_cost = 1, order_rate = 10, seed = 1)
+                NUM_STATES += 1
+                NUM_ACTIONS += 1
+                NUM_REWARDS = env.max_r - env.min_r + 1
+                model = DiscreteRewardDynamicsModel(NUM_STATES, NUM_ACTIONS, NUM_REWARDS, env.min_r)
 
-        elif ENV_NAME == "inventory":
-            env = Inventory(inventory_size = NUM_STATES, fixed_cost = 1, order_rate = 10, seed = 1)
-            NUM_STATES += 1
-            NUM_ACTIONS += 1
-            NUM_REWARDS = env.max_r - env.min_r + 1
-            model = DiscreteRewardDynamicsModel(NUM_STATES, NUM_ACTIONS, NUM_REWARDS, env.min_r)
+                # Compute optimal policy with VI
+                VI_v, VI_pi = value_iteration(env, 0.95)
+                behaviour_policy = EpsilonGreedyPolicy(VI_pi, EPSILON, NUM_ACTIONS)
 
-            # Compute optimal policy with VI
-            VI_v, VI_pi = value_iteration(env, 0.95)
-            behaviour_policy = EpsilonGreedyPolicy(VI_pi, EPSILON, NUM_ACTIONS)
+                greedy_policy = EpsilonGreedyPolicy(VI_pi, 0, NUM_ACTIONS)
 
-            greedy_policy = EpsilonGreedyPolicy(VI_pi, 0, NUM_ACTIONS)
+                # Uniform policy
+                pi_star_probs = np.ones(shape=(NUM_STATES,NUM_ACTIONS))/NUM_ACTIONS
+                pi_uniform = TableBasedPolicy(pi_star_probs)
 
-            # Uniform policy
-            pi_star_probs = np.ones(shape=(NUM_STATES,NUM_ACTIONS))/NUM_ACTIONS
-            pi_uniform = TableBasedPolicy(pi_star_probs)
+            #Collect experience data using behaviour policy and train model
+            model, dataset = get_data(env, N_TRAJECTORIES, behaviour_policy, model, HORIZON, path)
 
-        #Collect experience data using behaviour policy and train model
-        model, dataset = get_data(env, N_TRAJECTORIES, behaviour_policy, model, HORIZON, path)
+            #Split dataset into training (90%) and calibration data (10%)
+            calibration_trajectories = N_TRAJECTORIES // 10
+            data_tr = dataset[:N_TRAJECTORIES - calibration_trajectories]
+            data_cal = dataset[N_TRAJECTORIES - calibration_trajectories:N_TRAJECTORIES]
+            test_state = np.random.randint(env.ns)
 
-        #Split dataset into training (90%) and calibration data (10%)
-        calibration_trajectories = N_TRAJECTORIES // 10
-        data_tr = dataset[:N_TRAJECTORIES - calibration_trajectories]
-        data_cal = dataset[N_TRAJECTORIES - calibration_trajectories:N_TRAJECTORIES]
-        test_state = np.random.randint(env.ns)
+            #Train quantile predictors using training dataset
+            print('> Training/loading quantile networks')
+            upper_quantile_net = MLP(1, 64, 1, False)
+            lower_quantile_net = MLP(1, 64, 1, False)
 
-        #Train quantile predictors using training dataset
-        print('> Training/loading quantile networks')
-        upper_quantile_net = MLP(1, 64, 1, False)
-        lower_quantile_net = MLP(1, 64, 1, False)
+            if not lower_quantile_net.load(path + 'data/networks/lower_quantile_net.pth'):
+                y_avg, y_std = train_predictor(lower_quantile_net, data_tr, epochs=EPOCHS, quantile=QUANTILE/2, lr=LR, momentum=MOMENTUM)
+                lower_quantile_net.set_normalization(y_avg, y_std)
+                os.makedirs(os.path.dirname(path + 'data/networks/lower_quantile_net.pth'), exist_ok=True)
+                lower_quantile_net.save(path + 'data/networks/lower_quantile_net.pth')
 
-        if not lower_quantile_net.load(path + 'data/networks/lower_quantile_net.pth'):
-            y_avg, y_std = train_predictor(lower_quantile_net, data_tr, epochs=EPOCHS, quantile=QUANTILE/2, lr=LR, momentum=MOMENTUM)
-            lower_quantile_net.set_normalization(y_avg, y_std)
-            os.makedirs(os.path.dirname(path + 'data/networks/lower_quantile_net.pth'), exist_ok=True)
-            lower_quantile_net.save(path + 'data/networks/lower_quantile_net.pth')
-
-        if not upper_quantile_net.load(path + 'data/networks/upper_quantile_net.pth'):
-            y_avg, y_std = train_predictor(upper_quantile_net, data_tr, epochs=EPOCHS, quantile=1-(QUANTILE/2), lr=LR, momentum=MOMENTUM)
-            upper_quantile_net.set_normalization(y_avg, y_std)
-            os.makedirs(os.path.dirname(path + 'data/networks/upper_quantile_net.pth'), exist_ok=True)
-            upper_quantile_net.save(path + 'data/networks/upper_quantile_net.pth')
+            if not upper_quantile_net.load(path + 'data/networks/upper_quantile_net.pth'):
+                y_avg, y_std = train_predictor(upper_quantile_net, data_tr, epochs=EPOCHS, quantile=1-(QUANTILE/2), lr=LR, momentum=MOMENTUM)
+                upper_quantile_net.set_normalization(y_avg, y_std)
+                os.makedirs(os.path.dirname(path + 'data/networks/upper_quantile_net.pth'), exist_ok=True)
+                upper_quantile_net.save(path + 'data/networks/upper_quantile_net.pth')
 
 
-        epsilons = np.linspace(0, 1, 10)
-        epsilon_lengths = []
-        for epsilon_value in epsilons:
+            epsilons = np.linspace(0, 1, 10)
+            epsilon_lengths = []
+            for epsilon_value in epsilons:
 
-            pi_target = MixedPolicy(pi_uniform, greedy_policy, epsilon_value)
+                pi_target = MixedPolicy(pi_uniform, greedy_policy, epsilon_value)
 
-            test_points = collect_exp(env, 100, HORIZON, pi_target, None, test_state)
+                test_points = collect_exp(env, 100, HORIZON, pi_target, None, test_state)
 
-            print(f'> Estimate weights for calibration data')
-            weights_estimator = WeightsEstimator(behaviour_policy, pi_target, lower_quantile_net, upper_quantile_net)
-            if GRADIENT_BASED:
-                if TRANSFORMER: 
-                    scores, weights, weight_network = weights_estimator.gradient_method(data_tr, data_cal, LR, EPOCHS, lambda:WeightsTransformerMLP(2 + 2*NUM_STATES*NUM_ACTIONS, 64, 1, upper_quantile_net.mean, upper_quantile_net.std, behaviour_policy, pi_target))
-                else: 
-                    scores, weights, weight_network = weights_estimator.gradient_method(data_tr, data_cal, LR, EPOCHS, lambda:WeightsMLP(2, 64, 1, upper_quantile_net.mean, upper_quantile_net.std))
-            else:
-                scores, weights = weights_estimator.model_based(data_tr, data_cal, HORIZON, model, N_CPU)
-                weight_network = None
+                print(f'> Estimate weights for calibration data')
+                weights_estimator = WeightsEstimator(behaviour_policy, pi_target, lower_quantile_net, upper_quantile_net)
+                if GRADIENT_BASED:
+                    if TRANSFORMER: 
+                        scores, weights, weight_network = weights_estimator.gradient_method(data_tr, data_cal, LR, EPOCHS, lambda:WeightsTransformerMLP(2 + 2*NUM_STATES*NUM_ACTIONS, 64, 1, upper_quantile_net.mean, upper_quantile_net.std, behaviour_policy, pi_target))
+                    else: 
+                        scores, weights, weight_network = weights_estimator.gradient_method(data_tr, data_cal, LR, EPOCHS, lambda:WeightsMLP(2, 64, 1, upper_quantile_net.mean, upper_quantile_net.std))
+                else:
+                    scores, weights = weights_estimator.model_based(data_tr, data_cal, HORIZON, model, N_CPU)
+                    weight_network = None
 
-            # Generate y values for test point
-            print(f'> Computing conformal set')
-            conformal_set = ConformalSet(lower_quantile_net, upper_quantile_net, behaviour_policy, pi_target, model, HORIZON)
-            intervals, lower_quantile, upper_quantile = conformal_set.build_set(test_points, weights, scores, N_CPU, weight_network, GRADIENT_BASED)
+                # Generate y values for test point
+                print(f'> Computing conformal set')
+                conformal_set = ConformalSet(lower_quantile_net, upper_quantile_net, behaviour_policy, pi_target, model, HORIZON)
+                intervals, lower_quantile, upper_quantile = conformal_set.build_set(test_points, weights, scores, N_CPU, weight_network, GRADIENT_BASED)
 
-            included = 0
-            lengths = []
-            true_values = []
-            for interval in intervals:
-                if interval[-1] >= interval[2] and interval[-1] <= interval[3]:
-                    included += 1
-                lengths.append(interval[3]-interval[2])
-                true_values.append(interval[-1])
+                included = 0
+                lengths = []
+                true_values = []
+                for interval in intervals:
+                    if interval[-1] >= interval[2] and interval[-1] <= interval[3]:
+                        included += 1
+                    lengths.append(interval[3]-interval[2])
+                    true_values.append(interval[-1])
 
-            included = included/len(intervals)
-            mean_length = np.mean(lengths)
-            epsilon_lengths.append(mean_length)
+                included = included/len(intervals)
+                mean_length = np.mean(lengths)
+                epsilon_lengths.append(mean_length)
 
-            print("Epsilon: {} | Coverage: {:.2f}% | Average interval length: {} | Original interval: {}-{}".format(epsilon_value, included*100, mean_length, lower_quantile, upper_quantile))
-            file_logger.write([epsilon_value, included*100, mean_length, lower_quantile, upper_quantile, HORIZON, EPSILON])
+                print("Epsilon: {} | Coverage: {:.2f}% | Average interval length: {} | Original interval: {}-{}".format(epsilon_value, included*100, mean_length, lower_quantile, upper_quantile))
+                file_logger.write([epsilon_value, included*100, mean_length, lower_quantile, upper_quantile, HORIZON, EPSILON])
